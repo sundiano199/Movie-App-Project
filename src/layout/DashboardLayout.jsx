1db1746888ecb732b53ac1d0103c28019bd49c23
sunday <iamdyclef@gmail.com>
2025-09-24 17:13:12 +0100

    feat:still working

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -8,35 +8,51 @@ import tv from "../assets/tv.png";
 import bookmark from "../assets/bookmark.png";
 import profile from "../assets/profile.png";
 import search from "../assets/search.png";
+import profile2 from "../assets/profile2.png"
+
 const DashboardLayout = () => {
   return (
-    <div className="flex">
-      <div className="grid  my-[32px] ml-[32px] mr-[36px] bg-[#161D2F] rounded-[20px]">
-        <div>
+    <div className="md:flex  ">
+      <div className="flex justify-between md:flex-col  md:my-[32px] md:ml-[32px] md:mr-[36px] bg-[#161D2F] rounded-none md:rounded-[20px] md:px-[28px] px-[16px] py-[18px] md:py-0 ">
+        <div className="">
           <Link to="/">
             <img
               src={Logo}
               alt="Logo"
-              className="block mx-auto pt-[35px] px-[32px] pb-[75px]"
+              className="md:flex md:mt-[35px] md:mx-auto md:mb-[75px] "
             />{" "}
           </Link>
         </div>
-        <div>
-          <img src={menu} alt="" className="px-[38px] pb-[40px]" />
-          <img src={movies} alt="" className="px-[38px] pb-[40px]" />
-          <img src={tv} alt="" className="px-[38px] pb-[40px]" />
-          <img src={bookmark} alt="" className="px-[38px] pb-[552px]" />
+        <div
+          className="flex justify-between items-center
+         gap-5 md:block"
+        >
+          <img src={menu} alt="" className="md:mx-auto md:pb-[40px]" />
+          <img src={movies} alt="" className="md:mx-auto md:pb-[40px]" />
+          <img src={tv} alt="" className="md:mx-auto md:pb-[40px]" />
+          <img src={bookmark} alt="" className="md:mx-auto md:pb-[552px]" />
         </div>
-        <div>
-          <img src={profile} alt="" className="px-[28px] pb-[32px]" />
+        <div className="">
+          <img src={profile} alt="" className="h-6 md:hidden md:pb-[32px]" />
+          <img
+            src={profile2}
+            alt=""
+            className="hidden md:block  md:pb-[32px]"
+          />
         </div>
       </div>
-      <div>
-        <div>
-          <img src="" alt="" />
-          <input type="text" placeholder="Search for movies or TV series" />
+      <div className="md:mt-[64px] w-full">
+        <div className="flex gap-3 items-center md:my-0 my-[24px] md:px-0 px-[16px]">
+          <img src={search} alt="" />
+          <input
+            type="text"
+            placeholder="Search for movies or TV series"
+            className="w-100 placeholder:text-[16px]  md:placeholder:text-[24px] placeholder:font-light"
+          />
         </div>
-        <Outlet />
+        <div className="">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
-  )
+    <div className="flex flex-col justify-center items-center text-center leading-loose h-screen">
+      <h1 className="text-6xl">OOOps!!</h1>
+      <h2 className="text-md">404-Page Not Found</h2>
+      <Link to="/">
+        <button className="bg-white text-black rounded-lg p-2 text-xs cursor-pointer">
+          GOTO HOMEPAGE
+        </button>
+      </Link>
+    </div>
+  );
 }
 
 export default Error404
